8d0b08777c9a01bc39a2a349c722c32d4a72c2a2
Raul <rp519@ic.ac.uk>
2021-05-16 17:50:00 +0100

    updated gitignore

--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Edit <code>src/views/Options/App.js</code> and save.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
